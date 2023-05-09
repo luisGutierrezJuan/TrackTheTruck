15f5c088d57e24b4fd9237316ccf021d28e60a52
DerKom <mac_alicante@hotmail.com>
2023-05-09 11:24:48 +0100

    creation of the main project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RestaurantsComponent } from './pages/restaurants/restaurants.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    RestaurantsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
